f572239c5d1e409021ba43f0ba36888cdfff0117
provideforme <evan.a.bernard@gmail.com>
2022-01-28 22:24:20 -0500

    complete throught task 3

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// ## Features
+
+// - Allow the player to continually be prompted to enter their guess at the secret number until they guess correctly.
+// - If the player has an incorrect guess, display an alert message that informs the player:
+//     - Whether their guess is too high or too low, and…
+//     - A list of all the previously guessed numbers (without showing the square brackets of an array).
+// - If the player has guessed the secret number:
+//     - Display an alert message that congrats the player and informs them of how many guesses they took.
+//     - End the gameplay.
+
 
 
 const game = {
@@ -5,8 +15,50 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
+  
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+
+      return this.secretNum
   }
+
+  
+  
+  
+} 
+
+
+const getGuess = parseInt(prompt(`Enter a guess between ${game.smallestNum} and ${game.biggestNum}`));
+
+while(getGuess !== game.play()){
+  alert(`shit on my dick`)
 }
+
+if(getGuess === game.play()){
+  alert(
+    `Congrats! You guessed the number in`
+  )
+}
+
+// if (getGuess.play() == secretNum) {
+//   alert("Wow! I'm a Scorpio too!");
+
+
+
+
+
+
+
+
+// if(getGuess === this.secretNum){
+//   return 
+// }
+
+// console.log(game.play())
+
+// if(i =)
+
+
+// console.log()
